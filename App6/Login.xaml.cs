fb4e8ed4b87b7010c7d2d127a6331496ffbb53ba
carmanti <mantilla.carlosjoselo@gmail.com>
2022-12-07 10:44:01 -0500

    cambios

---
+++ App6/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double dato1 =Convert.ToDouble(txtDatoUno.Text);
+            double dato2 =Convert.ToDouble(txtDatoDos.Text);
+            double dato3 =Convert.ToDouble(txtDatoTres.Text);
+            double operacion = dato1 + dato2 + dato3;
+            txtResultado.Text = operacion.ToString();
+        }
+
+
+
+    }
+}
